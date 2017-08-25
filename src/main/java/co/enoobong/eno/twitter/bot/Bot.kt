56370b3e644c2780de17e7c7860a7048ffe3f0b5
Ibanga Enoobong Ime <enoimeibanga@gmail.com>
2017-08-25 19:33:51 +0100

    Update Bot.kt

--- src/main/java/co/enoobong/eno/twitter/bot/Bot.kt
+++ src/main/java/co/enoobong/eno/twitter/bot/Bot.kt
@@ -91,7 +91,7 @@ class Bot : ServletContextListener {
         twitterStream.addListenerFixed(listener)
         val filter = FilterQuery()
         //TODO externalize interests
-        val track = arrayOf("#forLoopAbuja", "#Kotlin", "#JavaOne", "#TalesFromPitchDrive", "#JavaEE8", "#ThisIsAndela")
+        val track = arrayOf("#forLoopAbuja", "#Kotlin", "#JavaOne", "#TalesFromPitchDrive", "#ThisIsAndela")
         filter.track(*track).language("en")
 
         twitterStream.filter(filter)
