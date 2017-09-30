69b07afcb68ab565e8272287ced035460b38d1f3
Ibanga Enoobong Ime <enoimeibanga@gmail.com>
2017-09-30 13:04:21 +0100

    Update Bot.kt
    Retweet forLoopLagos

--- src/main/java/co/enoobong/eno/twitter/bot/Bot.kt
+++ src/main/java/co/enoobong/eno/twitter/bot/Bot.kt
@@ -40,7 +40,7 @@ class Bot : ServletContextListener {
 
     override fun contextInitialized(sce: ServletContextEvent) {
         println("Started up!")
-        //retweetMyInterests()
+        retweetMyInterests()
         scheduler.scheduleAtFixedRate({ retweetMyMentions() }, 0, 15, TimeUnit.MINUTES)
     }
 
@@ -91,7 +91,7 @@ class Bot : ServletContextListener {
         twitterStream.addListenerFixed(listener)
         val filter = FilterQuery()
         //TODO externalize interests
-        val track = arrayOf("#forLoopAbuja", "#JavaOne", "#TalesFromPitchDrive", "#ThisIsAndela")
+        val track = arrayOf("#forLoopLagos", "#NextBillionUsers", "#ThisIsAndela")
         filter.track(*track).language("en")
 
         twitterStream.filter(filter)
